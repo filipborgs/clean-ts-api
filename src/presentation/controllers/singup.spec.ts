95c80321d5660ce40a1c14b684529b80f13aa3c4
Filipe Borges <filipborgs48@gmail.com>
2022-03-11 22:34:23 -0300

    test: refactor tests to use faker and beforeEach httpRequest

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -1,18 +1,28 @@
 import { SingUpController } from './singup'
+import faker from '@faker-js/faker'
 
 describe('SingUp Controller', () => {
-  test('should return if no name is provided', () => {
-    const sut = new SingUpController()
-    const httpRequest = {
+  let httpRequest: any
+  let sut: SingUpController
+
+  beforeEach(() => {
+    sut = new SingUpController()
+    const password = faker.internet.password()
+    httpRequest = {
       body: {
-        email: '',
-        password: '',
-        passwordConfirmation: ''
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        password,
+        passwordConfirmation: password
       }
     }
+  })
+
+  test('should return if no name is provided', () => {
+    delete httpRequest.body.name
     const httpResponse = sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error(''))
+    expect(httpResponse.body).toEqual(new Error('Missing required param: name'))
   })
 })
