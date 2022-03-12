2fd227e9307dcc72f424b25410073cd9f5fca9c9
Filipe Borges <filipborgs48@gmail.com>
2022-03-12 00:16:37 -0300

    test: ensure SingUpController return 400 if an invalid email is provided

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -2,13 +2,23 @@ import { SingUpController } from './singup'
 import faker from '@faker-js/faker'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../erros/missing-param-error'
+import { InvalidParamError } from '../erros/invalid-param-error'
+import { EmailValidator } from '../protocols/email-validator'
 
 describe('SingUp Controller', () => {
   let httpRequest: HttpRequest
   let sut: SingUpController
+  let emailValidatorStub: EmailValidator
 
   beforeEach(() => {
-    sut = new SingUpController()
+    class EmailValidatorStub implements EmailValidator {
+      isValid (email: string): boolean {
+        return true
+      }
+    }
+    emailValidatorStub = new EmailValidatorStub()
+    sut = new SingUpController(emailValidatorStub)
+
     const password = faker.internet.password()
     httpRequest = {
       body: {
@@ -50,6 +60,18 @@ describe('SingUp Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
+
+  test('should return 400 if no an email is provided', () => {
+    httpRequest.body.email = 'invalidEmail'
+
+    const emailValidatorSpy = jest.spyOn(emailValidatorStub, 'isValid').mockReturnValue(false)
+
+    const httpResponse: HttpResponse = sut.handle(httpRequest)
+
+    expect(emailValidatorSpy).toBeCalledWith(httpRequest.body.email)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
 
   test('should return 202 if request has no error', () => {
