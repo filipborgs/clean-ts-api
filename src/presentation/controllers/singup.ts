6446fa9acf7a52c4608658e7dc3355533efbffc9
Filipe Borges <filipborgs48@gmail.com>
2022-03-12 21:34:14 -0300

    feat: ensure SingUpController calls addAccount use case

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,26 +1,30 @@
+import { IAddAccountUseCase } from '../../use-cases/protocols/add-account'
 import { InvalidParamError, MissingParamError } from '../erros'
 import { badRequest, serverError } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse, Controller, EmailValidator } from '../protocols'
 
 export class SingUpController implements Controller {
-  constructor (private readonly emailValidator: EmailValidator) {}
+  constructor (
+    private readonly emailValidator: EmailValidator,
+    private readonly addAccountUseCase: IAddAccountUseCase) {}
 
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
-      const payload = httpRequest.body
+      const { name, email, password, passwordConfirmation } = httpRequest.body
 
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
-        if (!payload[field]) { return badRequest(new MissingParamError(field)) }
+        if (!httpRequest.body[field]) { return badRequest(new MissingParamError(field)) }
       }
 
-      if (payload.password !== payload.passwordConfirmation) {
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
-      if (!this.emailValidator.isValid(payload.email)) {
+      if (!this.emailValidator.isValid(email)) {
         return badRequest(new InvalidParamError('email'))
       }
+      this.addAccountUseCase.addAccount({ name, email, password })
 
       return {
         statusCode: 202
---
+++ src/use-cases/protocols/add-account.ts
@@ -0,0 +1,3 @@
+export interface IAddAccountUseCase {
+  addAccount: (data: any) => boolean
+}
