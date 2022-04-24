bed1faa9e7ca8430173c35f11747ead2c4cda205
Filipe Borges <filipborgs48@gmail.com>
2022-04-24 12:57:23 -0300

    feat: ensure DbAddAccount calls AddAccount Repository with correct values

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../domain/models'
 export * from '../../../domain/use-cases'
 export * from '../../protocols/encrypter'
+export * from '../../protocols/add-account-repository'
--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,10 +1,16 @@
-import { AddAccount, AddAccountModel, AccountModel, Encrypter } from './db-add-account-protocols'
+import { AddAccount, AddAccountModel, AccountModel, Encrypter, AddAccountRepository } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
-  constructor (private readonly encrypter: Encrypter) {}
+  constructor (private readonly encrypter: Encrypter,
+    private readonly addAccountRepository: AddAccountRepository) {}
 
   async add (data: AddAccountModel): Promise<AccountModel> {
-    void await this.encrypter.encrypt(data.password)
-    return { id: 'null', ...data }
+    const password = await this.encrypter.encrypt(data.password)
+    const account = await this.addAccountRepository.add({
+      name: data.name,
+      email: data.email,
+      password
+    })
+    return account
   }
 }
