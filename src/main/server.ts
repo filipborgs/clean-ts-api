5a411c0841d052d3145c12277713dd542b8cc711
Filipe Borges <filipborgs48@gmail.com>
2022-04-25 21:45:34 -0300

    feat: ensure mongoDB load before app

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,7 @@
-import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
 
-app.listen(3000, () => console.log('Server is running 3000'))
+void MongoHelper.connect(env.mongoUrl).then(async () => {
+  const app = (await import('./config/app')).default
+  app.listen(env.port, () => console.log(`Server is running ${env.port}`))
+})
