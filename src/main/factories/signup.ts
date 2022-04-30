64eda407935b7ee2fc76c248d0c952dff49e14e6
Filipe Borges <filipborgs48@gmail.com>
2022-04-30 18:53:59 -0300

    feat: ensure LogControllerDecorator call controller handle

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,8 +2,13 @@ import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { SingUpController } from '../../presentation/controllers/singup/singup'
+import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSignUpController = (): SingUpController => new SingUpController(
-  new EmailValidatorAdapter(),
-  new DbAddAccount(new BcryptAdapter(12), new AccountMongoRepository()))
+export const makeSignUpController = (): Controller => {
+  const singUpController = new SingUpController(
+    new EmailValidatorAdapter(),
+    new DbAddAccount(new BcryptAdapter(12), new AccountMongoRepository()))
+  return new LogControllerDecorator(singUpController)
+}
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,9 +1,18 @@
 import { ServerError } from '../erros'
 import { HttpResponse } from '../protocols'
 
+export const ok = (body: any): HttpResponse => ({
+  statusCode: 200,
+  body
+})
+
 export const created = (body: any): HttpResponse => ({
   statusCode: 201,
   body
+})
+
+export const noConetent = (): HttpResponse => ({
+  statusCode: 204
 })
 
 export const noContent = (): HttpResponse => ({ statusCode: 204 })
