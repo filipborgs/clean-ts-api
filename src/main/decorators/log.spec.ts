a0946f127951bb8198a936ddc5ef0352df047c2b
Filipe Borges <filipborgs48@gmail.com>
2022-04-30 18:59:21 -0300

    refactor: move sut creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,17 +1,33 @@
 import { ok } from '../../presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
+class ControllerStub implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    return ok('any_body')
+  }
+}
 
+const makeController = (): ControllerStub => {
+  const controllerStub = new ControllerStub()
+  return controllerStub
+}
+
+interface SutTypes{
+  sut: LogControllerDecorator
+  controllerStub: ControllerStub
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut, controllerStub
+  }
+}
 describe('LogController Decorator', () => {
   test('should call controller handle', async () => {
-    class ControllerStub implements Controller {
-      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        return ok('any_body')
-      }
-    }
-    const controllerStub = new ControllerStub()
+    const { sut, controllerStub } = makeSut()
     const controllerStubSpy = jest.spyOn(controllerStub, 'handle')
-    const sut = new LogControllerDecorator(controllerStub)
     const httpRequest: HttpRequest = {
       body: 'valid_body'
     }
