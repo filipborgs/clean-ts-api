e4474c8647cabfd8ac8d4fa4bdf83c10f2876d34
Filipe Borges <filipborgs48@gmail.com>
2022-04-30 21:41:08 -0300

    refactor: remove any type

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogMongoErrorRepository } from '../../infra/db/mongodb/log-repository/log'
 import { SingUpController } from '../../presentation/controllers/singup/singup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
@@ -10,5 +11,5 @@ export const makeSignUpController = (): Controller => {
   const singUpController = new SingUpController(
     new EmailValidatorAdapter(),
     new DbAddAccount(new BcryptAdapter(12), new AccountMongoRepository()))
-  return new LogControllerDecorator(singUpController, null as any)
+  return new LogControllerDecorator(singUpController, new LogMongoErrorRepository())
 }
