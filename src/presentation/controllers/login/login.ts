e77d3ed745cb5f0d9ad4b19e39552ca08dfcfb96
Filipe Borges <filipborgs48@gmail.com>
2022-05-01 13:47:14 -0300

    feat: sould returns 400 if no password is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -4,9 +4,12 @@ import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class LoginController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email } = httpRequest.body
+    const { email, password } = httpRequest.body
     if (!email) {
       return badRequest(new MissingParamError('email'))
+    }
+    if (!password) {
+      return badRequest(new MissingParamError('password'))
     }
     return null as any
   }
