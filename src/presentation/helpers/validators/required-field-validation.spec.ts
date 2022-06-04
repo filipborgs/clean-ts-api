613cc189f04a280932209230717ded4d8a21910d
Filipe <filipborgs48@gmail.com>
2022-06-04 15:10:21 -0300

    test: ensure RequiredFieldValidation not returns MissingParamError if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -2,9 +2,16 @@ import { MissingParamError } from '../../erros'
 import { RequiredFieldValidation } from './required-field-validation'
 
 describe('RequiredFieldValidation', () => {
+  const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation('field')
   test('Should returns MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({})
     expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should not returns MissingParamError if validation succeeds', () => {
+    const sut = makeSut()
+    const response = sut.validate({ field: 'any_value' })
+    expect(response).toBeFalsy()
   })
 })
