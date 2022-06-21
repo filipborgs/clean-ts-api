a28857af55c1e5f7c8370025687a749287bea0da
Filipe Borges <filipborgs48@gmail.com>
2022-06-21 20:52:19 -0300

    feat: ensure AddSurveyController call AddSurveyUseCAse with correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,9 +1,11 @@
 import { badRequest, noConetent, serverError } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, Validation } from '../../../protocols'
+import { AddSurvey } from './add-survey-protocols'
 
 export class AddSurveyController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -12,6 +14,8 @@ export class AddSurveyController implements Controller {
       if (error) {
         return badRequest(error)
       }
+      const { question, answers } = httpRequest.body
+      await this.addSurvey.add({ question, answers })
       return noConetent()
     } catch (error) {
       return serverError(error)
