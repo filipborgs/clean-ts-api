eb6a62b112a025c06b23db0fd27858ff393cad9f
Filipe Borges <filipborgs48@gmail.com>
2022-06-22 21:35:28 -0300

    test: ensure AccountMongoRepository find an Account with role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -85,5 +85,23 @@ describe('Account Mongo Repository', () => {
       expect(account?.password).toBe('any_password')
       expect(account?.accessToken).toBe('any_token')
     })
+
+    it('should return an account on loadByToken with role', async () => {
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'any_role'
+      })
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token', 'any_role')
+      expect(account).toBeTruthy()
+      expect(account?.id).toBeTruthy()
+      expect(account?.name).toBe('any_name')
+      expect(account?.email).toBe('any_email')
+      expect(account?.password).toBe('any_password')
+      expect(account?.accessToken).toBe('any_token')
+    })
   })
 })
