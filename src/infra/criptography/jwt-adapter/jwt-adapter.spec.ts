3b0f6b4a2c960d83f4c909fed331dea9433fef0e
Filipe Borges <filipborgs48@gmail.com>
2022-07-27 11:32:22 -0300

    test: implementation of decrypt on JwtAdapter

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -4,6 +4,9 @@ import jwt from 'jsonwebtoken'
 jest.mock('jsonwebtoken', () => ({
   sign: async (): Promise<string> => {
     return 'valid_token'
+  },
+  verify: async (): Promise<string> => {
+    return 'any_value'
   }
 }))
 
@@ -31,6 +34,29 @@ describe('JWT Adapter', () => {
       jest.spyOn(jwt, 'sign').mockImplementation(() => { throw new Error() })
       const sut = makeSut()
       const promise = sut.generate('any_id')
+      await expect(promise).rejects.toThrow()
+    })
+  })
+
+  describe('verify()', () => {
+    it('should call verify with correct values', async () => {
+      const param = 'value'
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt(param)
+      expect(verifySpy).toHaveBeenCalledWith(param, secret)
+    })
+
+    test('Should return a value on verify sucess', async () => {
+      const sut = makeSut()
+      const value = await sut.decrypt('any_token')
+      expect(value).toEqual('any_value')
+    })
+
+    it('shoud throw if verify throws', async () => {
+      jest.spyOn(jwt, 'verify').mockImplementation(() => { throw new Error() })
+      const sut = makeSut()
+      const promise = sut.decrypt('any_token')
       await expect(promise).rejects.toThrow()
     })
   })
