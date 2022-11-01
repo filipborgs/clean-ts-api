42b48ffac0b58c991db1fe380dd6397177099db7
Filipe Borges <filipborgs48@gmail.com>
2022-10-31 23:00:22 -0300

    fix: ensure AccountMongoRepository allows admin to access any route

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -19,7 +19,14 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
 
   async loadByToken (accessToken: string, role?: string): Promise<AccountModel | null> {
     const accountCollection = await MongoHelper.getCollection('accounts')
-    const account = await accountCollection.findOne({ accessToken, role })
+    const account = await accountCollection.findOne({
+      accessToken,
+      $or: [{
+        role
+      }, {
+        role: 'admin'
+      }]
+    })
     return MongoHelper.map(account)
   }
 
