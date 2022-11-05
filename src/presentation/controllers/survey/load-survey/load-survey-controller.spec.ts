63c710cadea82322e9e9c39c17d4467b5470f5a5
Filipe Borges <filipborgs48@gmail.com>
2022-11-05 17:42:28 -0300

    feat: ensure LoadSurveyController returns 500 if LoadSurvey throws

--- src/presentation/controllers/survey/load-survey/load-survey-controller.spec.ts
+++ src/presentation/controllers/survey/load-survey/load-survey-controller.spec.ts
@@ -1,22 +1,53 @@
 import { SurveyModel } from '../../../../domain/models/survey'
 import { LoadSurvey } from '../../../../domain/use-cases/survey/load-surveys'
 import { LoadSurveyController } from './load-survey-controller'
+import { serverError } from '../../../helpers/http/http-helper'
 
 describe('LoadSurveyController', () => {
   const makeLoadSurveyStub = (): LoadSurvey => {
     class LoadSurveyStub implements LoadSurvey {
       async load (): Promise<SurveyModel[] | null> {
-        return []
+        return makeFakeSurveys()
       }
     }
-
     return new LoadSurveyStub()
   }
-  test('Should call LoadSurvey with no values', async () => {
+
+  const makeFakeSurveys = (): SurveyModel[] => ([
+    {
+      id: 'any_id',
+      question: 'anu_question',
+      date: new Date(),
+      answers: []
+    }
+  ])
+
+  const makeSut = (): SutTypes => {
     const loadSurvey = makeLoadSurveyStub()
-    const loadSpy = jest.spyOn(loadSurvey, 'load')
     const sut = new LoadSurveyController(loadSurvey)
+    return {
+      loadSurvey,
+      sut
+    }
+  }
+
+  interface SutTypes {
+    loadSurvey: LoadSurvey
+    sut: LoadSurveyController
+  }
+
+  test('Should call LoadSurvey with no values', async () => {
+    const { sut, loadSurvey } = makeSut()
+    const loadSpy = jest.spyOn(loadSurvey, 'load')
     await sut.handle()
     expect(loadSpy).toBeCalledWith()
+  })
+
+  test('Should return 500 if LoadSurvey throws', async () => {
+    const { sut, loadSurvey } = makeSut()
+    const error = new Error('error')
+    jest.spyOn(loadSurvey, 'load').mockImplementation(() => { throw error })
+    const response = await sut.handle()
+    expect(response).toEqual(serverError(error))
   })
 })
--- src/presentation/controllers/survey/load-survey/load-survey-controller.ts
+++ src/presentation/controllers/survey/load-survey/load-survey-controller.ts
@@ -1,10 +1,15 @@
 import { LoadSurvey } from '../../../../domain/use-cases/survey/load-surveys'
+import { serverError } from '../../../helpers/http/http-helper'
 import { Controller, HttpResponse } from './load-survey-protocols'
 
 export class LoadSurveyController implements Controller {
   constructor (private readonly loadSurvey: LoadSurvey) {}
   async handle (): Promise<HttpResponse> {
-    await this.loadSurvey.load()
-    return null as any
+    try {
+      await this.loadSurvey.load()
+      return null as any
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
