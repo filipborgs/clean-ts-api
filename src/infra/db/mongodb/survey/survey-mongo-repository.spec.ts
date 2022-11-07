5fcca347317bbc259b0104d6f60ca35a51b2ee94
Filipe Borges <filipborgs48@gmail.com>
2022-11-07 20:55:12 -0300

    feat: ensure loadById return an survey on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -19,6 +19,20 @@ describe('SurveyMongoRepository', () => {
   const makeSut = (): SurveyMongoRepository => {
     return new SurveyMongoRepository()
   }
+
+  const makeFakeSurveys = (): any => ([
+    {
+      question: 'any_question',
+      date: new Date(),
+      answers: []
+    },
+    {
+      question: 'any_question2',
+      date: new Date(),
+      answers: []
+    }
+  ])
+
   describe('add', () => {
     it('should create an survey on success', async () => {
       const sut = makeSut()
@@ -36,19 +50,6 @@ describe('SurveyMongoRepository', () => {
   })
 
   describe('load', () => {
-    const makeFakeSurveys = (): any => ([
-      {
-        question: 'any_question',
-        date: new Date(),
-        answers: []
-      },
-      {
-        question: 'any_question2',
-        date: new Date(),
-        answers: []
-      }
-    ])
-
     test('Should return an array of surveys if succeeds', async () => {
       await surveyCollection.insertMany(makeFakeSurveys())
       const sut = makeSut()
@@ -67,6 +68,19 @@ describe('SurveyMongoRepository', () => {
       const sut = makeSut()
       const surveys = await sut.load()
       expect(surveys).toEqual([])
+    })
+  })
+
+  describe('loadById', () => {
+    test('Should return an survey on success', async () => {
+      const retorno = await surveyCollection.insertMany(makeFakeSurveys())
+      const id = retorno.insertedIds['0'].toString()
+      const sut = makeSut()
+      const survey = await sut.loadById(id)
+      expect(survey).toBeTruthy()
+      expect(survey.id).toBeTruthy()
+      expect(survey.date).toBeTruthy()
+      expect(survey.question).toEqual('any_question')
     })
   })
 })
--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,18 @@
 import { AddSurveyRepository } from '@/data/protocols/db/survey/add-survey'
 import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys'
+import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-surveys-by-id'
 import { SurveyModel } from '@/domain/models'
 import { AddSurveyModel } from '@/domain/use-cases'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { ObjectId } from 'mongodb'
 
-export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
+  async loadById (id: string): Promise<SurveyModel> {
+    const surveyCollection = await MongoHelper.getCollection('survey')
+    const survey = await surveyCollection.findOne({ _id: new ObjectId(id) })
+    return MongoHelper.map(survey)
+  }
+
   async load (): Promise<SurveyModel[]> {
     const surveyCollection = await MongoHelper.getCollection('survey')
     const surveys = await surveyCollection.find().toArray()
