67bd6bf4e21d4a8294ac36604e6b5d9531c80ba0
Filipe Borges <filipborgs48@gmail.com>
2022-11-07 17:58:59 -0300

    refactor: small improvements

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://teste:teste@mongo:27017/clean-ts-api',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://teste:teste@localhost:27017/clean-ts-api',
   port: process.env.PORT ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? 'secret'
 }
